469da3666c66542ba981e56ac97d073dda0f1bd9
arfanamd <arfanachmad11@gmail.com>
2024-06-30 15:03:01 +0700

    [Rust] bitmap-render update

--- Rust/clock/bitmap-render.rs
+++ Rust/clock/bitmap-render.rs
@@ -1,19 +1,26 @@
 #![allow(dead_code)]
 #![allow(unused)] 
 
-fn main() {
-	let num: u32 = 516898014;
-	let col: u8  = 6;
-	let row: u8  = 5;
-	
-	for y in 0..row {
-		for x in 0..col {
+fn render(col: &u8, row: &u8, num: &u32) {
+	for y in 0..*row {
+		for x in 0..*col {
 			if ((num >> (row - y - 1) * col + x) & 1) != 0 {
 				print!("*");
 			} else {
 				print!(" ");
 			}
 		}
+		println!("");
+	}
+}
+
+fn main() {
+	let col: u8        = 6;
+	let row: u8        = 5;
+	let map: [u32; 04] = [516898014, 1009828671, 516711231, 516717790];
+
+	for i in 0..4 {
+		render(&col, &row, &map[i]);
 		println!("");
 	}
 	/*
